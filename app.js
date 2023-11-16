4e739cd6f30863b3a66304376f557e28b224efc8
JulienPoirierWebDev <connexion@julienpoirier-Webdev.com>
2023-11-16 16:56:54 +0100

    feature: add search and infos routes for movie and serie

---
+++ app.js
@@ -0,0 +1,123 @@
+require("dotenv").config();
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+app.get("/search/movie/:qmovie/:page?", async (req, res) => {
+  try {
+    const movie = req.params.qmovie;
+    const page = req.params.page;
+    const TOKEN = process.env.TMDB_TOKEN;
+    const url = `https://api.themoviedb.org/3/search/movie?include_adult=false&language=fr-FR&page=${
+      page ? page : 1
+    }&query=${movie}`;
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    };
+
+    const response = await fetch(url, options);
+    const request = await response.json();
+    res.status(200).json(request.results);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error while getting the movies");
+  }
+});
+
+app.get("/search/serie/:q", async (req, res) => {
+  try {
+    const serie = req.params.q;
+    const TOKEN = process.env.TMDB_TOKEN;
+    const url = `https://api.themoviedb.org/3/search/tv?include_adult=false&language=fr-FR&page=1&query=${serie}`;
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    };
+
+    const response = await fetch(url, options);
+    const request = await response.json();
+    res.status(200).json(request.results);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error while getting the movies");
+  }
+});
+
+app.get("/infos/movie/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const TOKEN = process.env.TMDB_TOKEN;
+    const url = `https://api.themoviedb.org/3/movie/${id}?language=fr-FR`;
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    };
+
+    const response = await fetch(url, options);
+    const request = await response.json();
+    res.status(200).json(request);
+  } catch (err) {
+    res.status(500).send("Error while getting the movies");
+  }
+});
+
+app.get("/infos/serie/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const TOKEN = process.env.TMDB_TOKEN;
+    const url = `https://api.themoviedb.org/3/tv/${id}?language=fr-FR`;
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    };
+
+    const response = await fetch(url, options);
+    const request = await response.json();
+    res.status(200).json(request);
+  } catch (err) {
+    res.status(500).send("Error while getting the movies");
+  }
+});
+
+app.get("/infos/collection/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const TOKEN = process.env.TMDB_TOKEN;
+    const url = `https://api.themoviedb.org/3/collection/${id}?language=fr-FR`;
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    };
+
+    const response = await fetch(url, options);
+    const request = await response.json();
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error while getting the movies");
+  }
+});
+
+app.use((req, res) => {
+  res.status(404).send("Page not found");
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
