5e26772cf69d042397e3f4940e6b34bd23636187
JulienPoirierWebDev <connexion@julienpoirier-Webdev.com>
2023-11-23 15:35:34 +0100

    feat: add mongo and user register without hash

--- app.js
+++ app.js
@@ -6,10 +6,40 @@ const searchRouter = require("./routes/searchRouter");
 const infosRouter = require("./routes/infosRouter");
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const mongoose = require("mongoose");
+
+const databaseConnection = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/movies_api");
+    console.log("Success to connect MongoDB");
+  } catch (error) {
+    console.log("Failed to connect MongoDB");
+    console.log(error);
+  }
+};
+
+databaseConnection();
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  hash_password: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+const myUser = new User({
+  name: "Julien",
+  email: "test@hotmail.com",
+  hash_password: "1234",
+});
+
+myUser.save();
 
 app.use((req, res, next) => {
-  console.log(req.url);
-
   if (req.url === "/logs") {
     next();
     return;
@@ -26,7 +56,6 @@ app.use((req, res, next) => {
   try {
     fs.readFile("./log.json", (err, data) => {
       const log = JSON.parse(data);
-      console.log(log);
       log.use = log.use + 1;
       const newData = JSON.stringify(log);
 
@@ -47,6 +76,34 @@ app.get("/logs", async (req, res) => {
     const log = JSON.parse(data);
     res.status(200).json(log);
   });
+});
+
+app.post("/users/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400).json({ message: "Missing parameters" });
+    return;
+  }
+
+  const testIfUserWithThisEmailExist = await User.findOne({
+    email: email,
+  }).exec();
+
+  if (Boolean(testIfUserWithThisEmailExist)) {
+    res.status(400).json({ message: "User already exist" });
+    return;
+  }
+
+  const newUser = new User({
+    name,
+    email,
+    hash_password: password,
+  });
+
+  newUser.save();
+
+  res.status(201).json({ message: "User created" });
 });
 
 app.use("/search", searchRouter);
